f6fdad54749cf339f76e413a232fbbadc57f65b3
TrialNError <trialnerror@web.de>
2012-07-27 23:44:29 +0200

    GE-2: Foreign - irrklang einbinden
    - irrklang hochgeladen
    - Foreign.cmd erweitert

--- foreign/Foreign.cmd
+++ foreign/Foreign.cmd
@@ -1,8 +1,15 @@
 
 echo Erstelle und kopiere Dateien der Fremdkomponenten ...
 
+echo Beginne mit boost ...
 cd boost
 bjam -q --toolset=msvc --build-type=minimal --with-filesystem stage
 xcopy stage\lib\*.lib ..\..\%1 /i /y /q
+echo boost beendet.
+
+echo Beginne mit irrklang ...
+cd ../irrklang
+xcopy lib\irrklang.lib ..\..\%1 /i /y /q
+echo irrklang beendet.
 
 echo Dateien der Fremdkomponenten wurden erstellt und kopiert.
