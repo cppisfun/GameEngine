80fd9e82186e19b7f966acaed410d74d5ddf9a80
TrialNError <trialnerror@web.de>
2012-07-27 23:06:36 +0200

    GE-1: Foreign - boost 1.50 einbinden / GE-20: bin, lib -> Debug, Release

--- foreign/Foreign.cmd
+++ foreign/Foreign.cmd
@@ -3,6 +3,6 @@ echo Erstelle und kopiere Dateien der Fremdkomponenten ...
 
 cd boost
 bjam -q --toolset=msvc --build-type=minimal --with-filesystem stage
-xcopy stage\lib\*.lib ..\..\lib /i /y /q
+xcopy stage\lib\*.lib ..\..\%1 /i /y /q
 
 echo Dateien der Fremdkomponenten wurden erstellt und kopiert.
