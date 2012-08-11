de6393cd39a66014b6d8962143a479a69a9d3773
TrialNError <trialnerror@web.de>
2012-08-11 14:56:51 +0200

    Foreign - cmd erweitert um Irrlicht und irrKlang

--- foreign/Foreign.cmd
+++ foreign/Foreign.cmd
@@ -7,9 +7,16 @@ bjam -q --toolset=msvc --build-type=minimal --with-filesystem stage
 xcopy stage\lib\*.lib ..\..\%1 /i /y /q
 echo boost beendet.
 
-echo Beginne mit irrklang ...
-cd ../irrklang
+echo Beginne mit Irrlicht 1.7.3 ...
+cd ..\irrlicht
+xcopy lib\irrlicht.lib ..\..\%1 /i /y /q
+xcopy bin\Win32-VisualStudio\irrlicht.dll ..\..\%1 /i /y /q
+echo Irrlicht beendet.
+
+echo Beginne mit irrKlang 1.40 ...
+cd ..\irrklang
 xcopy lib\irrklang.lib ..\..\%1 /i /y /q
+xcopy bin\cpp\irrklang.dll ..\..\%1 /i /y /q
 echo irrklang beendet.
 
 echo Dateien der Fremdkomponenten wurden erstellt und kopiert.
