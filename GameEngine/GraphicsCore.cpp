4df5713c8f94864c5418f808b619dcce6b128c3f
TrialNError <trialnerror@web.de>
2012-09-01 00:27:21 +0200

    GE-29: Grafik - Funktionen zum elementaren Zeichnen

--- GameEngine/GraphicsCore.cpp
+++ GameEngine/GraphicsCore.cpp
@@ -40,9 +40,29 @@ void GraphicsCore::EndScene ()
    driver->endScene();
 }
 
+GraphicsCore& GraphicsCore::DrawPixel (int x, int y, const Color& color)
+{
+   driver->drawPixel(x, y, color.AsIrrColor());
+   return *this;
+}
+
+GraphicsCore& GraphicsCore::DrawLine (int left, int top, int right, int bottom, const Color& color)
+{
+   driver->draw2DLine(core::vector2d<int>(left, top), core::vector2d<int>(right, bottom), color.AsIrrColor());
+   return *this;
+}
+
+GraphicsCore& GraphicsCore::DrawRectangle (int left, int top, int right, int bottom, const Color& color, const DrawType& type)
+{
+   if (type == Outline) driver->draw2DRectangleOutline(core::rect<int>(left, top, right, bottom), color.AsIrrColor());
+   else                 driver->draw2DRectangle(color.AsIrrColor(), core::rect<int>(left, top, right, bottom));
+
+   return *this;
+}
+
 GraphicsCore& GraphicsCore::Text (int x, int y, const std::string& text)   // TODO
 {
-   gui->getBuiltInFont()->draw(base::AsWString(text).c_str(), core::rect<s32>(x, y, x + 100, y + 100), foreColor.AsIrrColor());
+   gui->getBuiltInFont()->draw(base::AsWString(text).c_str(), core::rect<int>(x, y, x + 100, y + 100), foreColor.AsIrrColor());
    return *this;
 }
 
--- GameEngine/GraphicsCore.h
+++ GameEngine/GraphicsCore.h
@@ -10,6 +10,13 @@
 
 class GEDLL GraphicsCore
 {
+public:
+   enum DrawType {
+      Fill = 0,
+      Outline
+   };
+
+private:
    irr::video::IVideoDriver* driver;
    irr::gui::IGUIEnvironment* gui;
 
@@ -29,6 +36,26 @@ public:
 
    void BeginScene ();
    void EndScene ();
+
+   GraphicsCore& DrawPixel (int x, int y, const Color& color);
+   GraphicsCore& DrawLine (int left, int top, int right, int bottom, const Color& color);
+   GraphicsCore& DrawRectangle (int left, int top, int right, int bottom, const Color& colorTopLeft, const Color& colorTopRight, const Color& colorBottomRight, const Color& colorBottomLeft, const DrawType& type);
+   GraphicsCore& DrawRectangle (int left, int top, int right, int bottom, const Color& color, const DrawType& type);
+
+   GraphicsCore& DrawBox (int left, int top, int right, int bottom, const Color& outlineColor, const Color& fillColor)
+   {
+      return DrawRectangle(left, top, right, bottom, fillColor, Fill).DrawRectangle(left, top, right, bottom, outlineColor, Outline);
+   }
+
+   GraphicsCore& DrawPixel (int x, int y)                            { return DrawPixel(x, y, foreColor); }
+   GraphicsCore& DrawLine (int left, int top, int right, int bottom) { return DrawLine(left, top, right, bottom, foreColor); }
+   GraphicsCore& DrawBox (int left, int top, int right, int bottom)  { return DrawBox(left, top, right, bottom, foreColor, backColor); }
+
+   GraphicsCore& DrawRectangle (int left, int top, int right, int bottom, const DrawType& type)
+   {
+      if (type == Outline) return DrawRectangle(left, top, right, bottom, foreColor, type);
+      else                 return DrawRectangle(left, top, right, bottom, backColor, type);
+   }
 
    GraphicsCore& Text (int x, int y, const std::string& text);
 
