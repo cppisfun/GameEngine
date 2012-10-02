284d723a7acf7dc259824634c99bb255cf737114
TrialNError <trialnerror@web.de>
2012-10-02 16:11:00 +0200

    GE-11: Input - InputGamepad implementieren (Zwischenstand-Commit)

--- GameEngine/InputGamepad.cpp
+++ GameEngine/InputGamepad.cpp
@@ -24,41 +24,62 @@ namespace ge {
    {
       if (eventCtrl == nullptr) throw error::NullPointer("Invalid event controller pointer!", __FUNCTION__);
       eventCtrl->GamepadCallback(std::bind(&InputGamepad::OnEvent, this, std::placeholders::_1));
+
+      std::fill(currButtons.begin(), currButtons.end(), false);
+      std::fill(prevButtons.begin(), prevButtons.end(), false);
    }
 
    void InputGamepad::OnEvent (const SEvent& event)
    {
       if (!Enabled()) return;
 
-      // TODO
+      for (int i = 0; i < buttonCount; ++i) {
+         currButtons[i] = event.JoystickEvent.IsButtonPressed(i);
+      }
    }
 
    void InputGamepad::Update ()
    {
       if (!Enabled()) return;
 
-      // TODO
+      std::copy(currButtons.begin(), currButtons.end(), prevButtons.begin());
    }
 
-   //InputGamepad::InputGamepad (IDirectInput8* input, HWND wnd) : enabled(true)
-   //{
-   //   Init(input, wnd);
-   //}
-   //
-   //InputGamepad::~InputGamepad ()
-   //{
-   //}
-   //
-   //void InputGamepad::Init (IDirectInput8* input, HWND wnd)
-   //{
-   //   if (input == nullptr)    throw error::NullPointer("Invalid input device!", __FUNCTION__);
-   //   else if (wnd == nullptr) throw error::NullPointer("Invalid window handle!", __FUNCTION__);
-   //}
-   //
-   //void InputGamepad::Update ()
-   //{
-   //   if (!enabled) return;
-   //}
+   bool InputGamepad::Button (int btn)
+   {
+      if (btn < NONE || btn >= buttonCount) throw error::InvalidParam("Button id out of range!", __FUNCTION__);
+      else if (btn > 0) return currButtons[btn];
+
+      for (int i = 0; i < buttonCount; ++i) {
+         if (currButtons[i]) return (btn == ANY);
+      }
+
+      return (btn == NONE);
+   }
+
+   bool InputGamepad::ButtonPressed (int btn)
+   {
+      if (btn < NONE || btn >= buttonCount) throw error::InvalidParam("Button id out of range!", __FUNCTION__);
+      else if (btn > 0) return (currButtons[btn] && !prevButtons[btn]);
+
+      for (int i = 0; i < buttonCount; ++i) {
+         if (currButtons[i] && !prevButtons[i]) return (btn == ANY);
+      }
+
+      return (btn == NONE);
+   }
+
+   bool InputGamepad::ButtonReleased (int btn)
+   {
+      if (btn < NONE || btn >= buttonCount) throw error::InvalidParam("Button id out of range!", __FUNCTION__);
+      else if (btn > 0) return (!currButtons[btn] && prevButtons[btn]);
+
+      for (int i = 0; i < buttonCount; ++i) {
+         if (!currButtons[i] && prevButtons[i]) return (btn == ANY);
+      }
+
+      return (btn == NONE);
+   }
 
 }
 
--- GameEngine/InputGamepad.h
+++ GameEngine/InputGamepad.h
@@ -12,6 +12,12 @@ namespace ge {
 
    class GEDLL InputGamepad : public EventListener
    {
+      static const int buttonCount = 32;
+      static const int axisCount   = 6;
+
+      std::array<bool, buttonCount> currButtons;
+      std::array<bool, buttonCount> prevButtons;
+
       void Init (EventController* eventCtrl);
 
       void OnEvent (const irr::SEvent& event);
@@ -21,6 +27,10 @@ namespace ge {
       virtual ~InputGamepad ();
 
       void Update ();
+
+      bool Button (int btn = ANY);
+      bool ButtonPressed (int btn = ANY);
+      bool ButtonReleased (int btn = ANY);
    };
 
 }
